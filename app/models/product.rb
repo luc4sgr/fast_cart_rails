939dc3528c043f02ed329ed2e8e710d9134cc2ed
luc4sgr <23.lucasdoliveira@gmail.com>
2025-03-20 19:00:34 -0300

    Add AdminLog and Notification models; update validations in User and Product models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,8 @@
 class Product < ApplicationRecord
     has_many :order_items, dependent: :destroy
     has_many :orders, through: :order_items
-  
+
     validates :name, presence: true
     validates :price, presence: true, numericality: { greater_than: 0 }
     validates :stock, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  end
-  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 
   # Método para verificar se o usuário é admin
