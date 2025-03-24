2a9b4ad23b308759a17e17ddf858e9dac383bd52
luc4sgr <23.lucasdoliveira@gmail.com>
2025-03-23 22:25:14 -0300

    Add validation for image_url presence in Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,5 @@ class Product < ApplicationRecord
     validates :name, presence: true
     validates :price, presence: true, numericality: { greater_than: 0 }
     validates :stock, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    validates :image_url, presence: true
 end
