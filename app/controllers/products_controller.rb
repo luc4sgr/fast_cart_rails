f8cf77d5e9071602ee902ef0c3bb1487945d981d
luc4sgr <23.lucasdoliveira@gmail.com>
2025-03-23 22:24:17 -0300

    Refactor ProductsController to skip authentication for index and show actions; streamline product retrieval logic

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,51 +1,21 @@
 class ProductsController < ApplicationController
-    before_action :set_product, only: [ :show, :update, :destroy ]
+  skip_before_action :authenticate_request, only: [:index, :show]
+  before_action :set_product, only: [:show]
 
-    # GET /products
-    def index
-      @products = Product.all
-      render json: @products
-    end
+  def index
+    products = Product.all
+    render json: products
+  end
 
-    # GET /products/:id
-    def show
-      render json: @product
-    end
+  def show
+    render json: @product
+  end
 
-    # POST /products
-    def create
-      @product = Product.new(product_params)
-      if @product.save
-        render json: @product, status: :created
-      else
-        render json: @product.errors, status: :unprocessable_entity
-      end
-    end
+  private
 
-    # PATCH/PUT /products/:id
-    def update
-      if @product.update(product_params)
-        render json: @product
-      else
-        render json: @product.errors, status: :unprocessable_entity
-      end
-    end
-
-    # DELETE /products/:id
-    def destroy
-      @product.destroy
-      head :no_content
-    end
-
-    private
-
-    def set_product
-      @product = Product.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: "Produto não encontrado" }, status: :not_found
-    end
-
-    def product_params
-      params.require(:product).permit(:name, :description, :price, :stock)
-    end
+  def set_product
+    @product = Product.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Produto não encontrado" }, status: :not_found
+  end
 end
