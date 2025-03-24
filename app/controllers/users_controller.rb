f991f718f764ddbec554d145fcdfe0d7a4c19335
luc4sgr <23.lucasdoliveira@gmail.com>
2025-03-23 22:24:26 -0300

    Refactor UsersController to add authorization for owner and admin; improve user creation logic and error handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,56 +1,65 @@
 class UsersController < ApplicationController
-    skip_before_action :authenticate_request, only: [ :create ]
-    before_action :set_user, only: [ :show, :update, :destroy ]
+  skip_before_action :authenticate_request, only: [:create]
+  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :authorize_owner_or_admin, only: [:show, :update, :destroy]
 
-    # GET /users
-    def index
-      @users = User.all
-      render json: @users
+  # GET /users
+  # (Opcional: se não quiser que clientes listem todos, pode remover)
+  def index
+    users = User.all
+    render json: users
+  end
+
+  # GET /users/:id
+  def show
+    render json: @user
+  end
+
+  # POST /users
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: user.errors, status: :unprocessable_entity
     end
+  end
 
-    # GET /users/:id
-    def show
+  # PATCH/PUT /users/:id
+  def update
+    if @user.update(user_params)
       render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
     end
+  end
 
-    # POST /users
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        render json: @user, status: :created
-      else
-        render json: @user.errors, status: :unprocessable_entity
-      end
+  # DELETE /users/:id
+  def destroy
+    @user.destroy
+    head :no_content
+  end
+
+  # GET /me
+  def me
+    render json: @current_user, status: :ok
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Usuário não encontrado" }, status: :not_found
+  end
+
+  def authorize_owner_or_admin
+    unless @current_user&.id == @user.id || @current_user&.admin?
+      render json: { error: "Acesso não autorizado" }, status: :forbidden
     end
+  end
 
-    # PATCH/PUT /users/:id
-    def update
-      if @user.update(user_params)
-        render json: @user
-      else
-        render json: @user.errors, status: :unprocessable_entity
-      end
-    end
-
-    # DELETE /users/:id
-    def destroy
-      @user.destroy
-      head :no_content
-    end
-
-    def me
-      render json: @current_user, status: :ok
-    end
-
-    private
-
-    def set_user
-      @user = User.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: "Usuário não encontrado" }, status: :not_found
-    end
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
